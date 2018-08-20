6b77e9a9b1f45fdb19239aae1b9bedd7284f478f
Franchini Mercado <franchini.mercado@meltwater.com>
2018-08-19 21:23:50 -0700

    Update spec for Item subclasses

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -44,164 +44,162 @@ describe('gileded rose - normal items', () => {
 
 describe('aged brie', () => {
   it ('decreases aged brie sellIn by 1', () => {
-    const brie = new Item('Aged Brie', 2, 0)
-    const updatedBrie = updateAgedBrie(brie)
+    const brie = new AgedBrie('Aged Brie', 2, 0)
+    brie.updateQuality(brie)
 
-    expect(updatedBrie.sellIn).toEqual(1)
+    expect(brie.sellIn).toEqual(1)
   })
 
   it ('increases aged brie quality by 1 if sellIn > 1', () => {
-    const brie = new Item('Aged Brie', 2, 0)
-    const updatedBrie = updateAgedBrie(brie)
+    const brie = new AgedBrie('Aged Brie', 2, 0)
+    brie.updateQuality(brie)
 
-    expect(updatedBrie.quality).toEqual(1)
+    expect(brie.quality).toEqual(1)
   })
 
   it ('increases aged brie quality by 1 if sellIn === 1', () => {
-    const brie = new Item('Aged Brie', 1, 0)
-    const updatedBrie = updateAgedBrie(brie)
+    const brie = new AgedBrie('Aged Brie', 1, 0)
+    brie.updateQuality(brie)
 
-    expect(updatedBrie.quality).toEqual(1)
+    expect(brie.quality).toEqual(1)
   })
 
   it ('increases aged brie quality by 2 if sellIn < 1', () => {
-    const brie = new Item('Aged Brie', 0, 0)
-    const updatedBrie = updateAgedBrie(brie)
+    const brie = new AgedBrie('Aged Brie', 0, 0)
+    brie.updateQuality(brie)
 
-    expect(updatedBrie.quality).toEqual(2)
+    expect(brie.quality).toEqual(2)
   })
 
   it ('does not change aged brie quality if quality === 50', () => {
-    const brie = new Item('Aged Brie', 2, 50)
-    const updatedBrie = updateAgedBrie(brie)
+    const brie = new AgedBrie('Aged Brie', 2, 50)
+    brie.updateQuality(brie)
 
-    expect(updatedBrie.quality).toEqual(50)
+    expect(brie.quality).toEqual(50)
   })
 })
 
 describe('sulfuras', () => {
   it('does not change sulfuras sellIn', () => {
-    const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80)
-    const updatedSulfuras = updateSulfuras(sulfuras)
+    const sulfuras = new Sulfuras('Sulfuras, Hand of Ragnaros', 0, 80)
 
-    expect(updatedSulfuras.sellIn).toEqual(0)
+    expect(sulfuras.sellIn).toEqual(0)
   })
 
   it('does not change sulfuras quality', () => {
     const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80)
-    const updatedSulfuras = updateSulfuras(sulfuras)
 
-    expect(updatedSulfuras.quality).toEqual(80)
+    expect(sulfuras.quality).toEqual(80)
   })
 })
 
 describe('backstage passes', () => {
   it('decreases backstage pass sellIn by 1', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', 15, 20)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.sellIn).toEqual(14)
+    expect(backstagePass.sellIn).toEqual(14)
   })
 
   it('increases backstage pass quality by 1 if sellIn > 10', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', 15, 20)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.quality).toEqual(21)
+    expect(backstagePass.quality).toEqual(21)
   })
 
   it('increases backstage pass quality by 2 if sellIn === 10', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', 10, 20)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.quality).toEqual(22)
+    expect(backstagePass.quality).toEqual(22)
   })
 
   it('increases backstage pass quality by 2 if sellIn <= 10 and sellIn > 5', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 7, 20)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', 7, 20)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.quality).toEqual(22)
+    expect(backstagePass.quality).toEqual(22)
   })
 
   it('increases backstage pass quality by 3 if sellIn === 5', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', 5, 20)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.quality).toEqual(23)
+    expect(backstagePass.quality).toEqual(23)
   })
 
   it('increases backstage pass quality by 3 if sellIn <= 5 and sellIn > 0', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 3, 20)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', 3, 20)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.quality).toEqual(23)
+    expect(backstagePass.quality).toEqual(23)
   })
 
   it('sets backstage pass quality to 0 if sellIn === 0', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', 0, 20)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.quality).toEqual(0)
+    expect(backstagePass.quality).toEqual(0)
   })
 
   it('sets backstage pass quality to 0 if sellIn < 0', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', -1, 20)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', -1, 20)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.quality).toEqual(0)
+    expect(backstagePass.quality).toEqual(0)
   })
 
   it('does not increase backstage pass quality if quality === 50', () => {
-    const backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 3, 50)
-    const updatedBackstagePass = updateBackstagePass(backstagePass)
+    const backstagePass = new BackstagePass('Backstage passes to a TAFKAL80ETC concert', 3, 50)
+    backstagePass.updateQuality(backstagePass)
 
-    expect(updatedBackstagePass.quality).toEqual(50)
+    expect(backstagePass.quality).toEqual(50)
   })
 })
 
 describe('conjured', () => {
   it('decreases conjured sellIn by 1', () => {
-    const conjured = new Item('Conjured Mana Cake', 3, 6)
-    const updatedConjured = updateConjured(conjured)
+    const conjured = new Conjured('Conjured Mana Cake', 3, 6)
+    conjured.updateQuality(conjured)
 
-    expect(updatedConjured.sellIn).toEqual(2)
+    expect(conjured.sellIn).toEqual(2)
   })
 
   it('decreases conjured quality by 2 if quality !== 0 and if sellIn is > 1', () => {
-    const conjured = new Item('Conjured Mana Cake', 3, 6)
-    const updatedConjured = updateConjured(conjured)
+    const conjured = new Conjured('Conjured Mana Cake', 3, 6)
+    conjured.updateQuality(conjured)
 
-    expect(updatedConjured.quality).toEqual(4)
+    expect(conjured.quality).toEqual(4)
   })
 
 
   it('decreases conjured quality by 2 if quality !== 0 and if sellIn is === 1', () => {
-    const conjured = new Item('Conjured Mana Cake', 1, 5)
-    const updatedConjured = updateConjured(conjured)
+    const conjured = new Conjured('Conjured Mana Cake', 1, 5)
+    conjured.updateQuality(conjured)
 
-    expect(updatedConjured.quality).toEqual(3)
+    expect(conjured.quality).toEqual(3)
   })
 
   it('decreases conjured quality by 4 if quality !== 0 and if sellIn is === 0', () => {
-    const conjured = new Item('Conjured Mana Cake', 0, 6)
-    const updatedConjured = updateConjured(conjured)
+    const conjured = new Conjured('Conjured Mana Cake', 0, 6)
+    conjured.updateQuality(conjured)
 
-    expect(updatedConjured.quality).toEqual(2)
+    expect(conjured.quality).toEqual(2)
   })
 
 
   it('decreases conjured quality by 4 if quality !== 0 and if sellIn is < 0', () => {
-    const conjured = new Item('Conjured Mana Cake', -1, 5)
-    const updatedConjured = updateConjured(conjured)
+    const conjured = new Conjured('Conjured Mana Cake', -1, 5)
+    conjured.updateQuality(conjured)
 
-    expect(updatedConjured.quality).toEqual(1)
+    expect(conjured.quality).toEqual(1)
   })
 
   it('does not decrease conjured quality if quality === 0', () => {
-    const conjured = new Item('Conjured Mana Cake', -1, 0)
-    const updatedConjured = updateConjured(conjured)
+    const conjured = new Conjured('Conjured Mana Cake', -1, 0)
+    conjured.updateQuality(conjured)
 
-    expect(updatedConjured.quality).toEqual(0)
+    expect(conjured.quality).toEqual(0)
   })
 })
