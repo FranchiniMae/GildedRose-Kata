5f4ba917fb61e859ee0a643fda8dc26dc5a4ce24
Franchini Mercado <franchini.mercado@meltwater.com>
2018-08-19 21:23:38 -0700

    Extend Item class

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -6,9 +6,78 @@ class Item {
   }
 }
 
+class AgedBrie extends Item {
+  constructor (name, sellIn, quality) {
+    super(name, sellIn, quality);
+
+    this._maxQuality = 50;
+  }
+
+  updateQuality () {
+    if (this.quality !== this._maxQuality) {
+      this.sellIn >= 1 ? this.quality += 1 : this.quality += 2;
+    }
+     this.sellIn -= 1;
+  }
+}
+
+class Sulfuras extends Item {
+  constructor (name, sellIn, quality) {
+    super (name, sellIn, quality);
+
+    this.quality = 80;
+  }
+}
+
+class BackstagePass extends Item {
+  constructor (name, sellIn, quality) {
+    super(name, sellIn, quality);
+
+    this._maxQuality = 50;
+    this._upperSellInLimit = 10;
+    this._midSellInLimit = 5;
+  }
+
+  updateQuality () {
+    if (this.quality !== this._maxQuality) {
+      if (this.sellIn > this._upperSellInLimit) {
+        this.quality += 1;
+      } else if (this.sellIn <= this._upperSellInLimit && this.sellIn > this._midSellInLimit) {
+        this.quality += 2;
+      } else if (this.sellIn <= this._midSellInLimit && this.sellIn > 0) {
+        this.quality += 3;
+      } else {
+        this.quality = 0;
+      }
+    }
+     this.sellIn -= 1;
+  }
+}
+
+class Conjured extends Item {
+  constructor (name, sellIn, quality) {
+    super (name, sellIn, quality);
+
+    this._minItemQuality = 0;
+  }
+
+  updateQuality () {
+    if (this.quality !== this._minItemQuality) {
+      this.sellIn >= 1 ? this.quality -= 2 : this.quality -= 4;
+    }
+     this.sellIn -= 1;
+  }
+}
+
 class Shop {
   constructor (items = []) {
     this.items = items;
+    this.specialItems = {
+      'Aged Brie': (item) => updateAgedBrie(item),
+      'Sulfuras, Hand of Ragnaros': (item ) => updatedSulfuras(item),
+      'Backstage passes to a TAFKAL80ETC concert': (item) => updateBackstagePass(item),
+      'Conjured Mana Cake': (item) => updateConjured(item)
+    }
   }
 
   updateNormalItems (item) {
@@ -24,8 +93,8 @@ class Shop {
 
   updateQuality() {
     this.items.forEach(item => {
-      if (SPECIAL_ITEMS[item.name]) {
-        SPECIAL_ITEMS[item.name](item);
+      if (this.specialItems[item.name]) {
+        this.specialItems[item.name](item);
       } else {
         this.updateNormalItems(item);
       }
@@ -33,59 +102,4 @@ class Shop {
 
     return this.items;
   }
-}
-
-// TODO: Move util functions to a util directory
-
-const SPECIAL_ITEMS = {
-  'Aged Brie': (item) => updateAgedBrie(item),
-  'Sulfuras, Hand of Ragnaros': (item ) => updatedSulfuras(item),
-  'Backstage passes to a TAFKAL80ETC concert': (item) => updateBackstagePass(item),
-  'Conjured Mana Cake': (item) => updateConjured(item)
-}
-
-const updateAgedBrie = (item) => {
-    const MAX_ITEM_QUALITY = 50;
-
-    if (item.quality !== MAX_ITEM_QUALITY) {
-      item.sellIn >= 1 ? item.quality += 1 : item.quality += 2;
-    }
-
-    item.sellIn -= 1;
-    return item;
-}
-
-const updateSulfuras = (item) => item
-
-const updateBackstagePass = (item) => {
-    const UPPER_SELL_IN_LIMIT = 10;
-    const MID_SELL_IN_LIMIT = 5;
-    const MAX_ITEM_QUALITY = 50;
-
-
-    if (item.quality !== MAX_ITEM_QUALITY) {
-      if (item.sellIn > UPPER_SELL_IN_LIMIT) {
-        item.quality += 1;
-      } else if (item.sellIn <= UPPER_SELL_IN_LIMIT && item.sellIn > MID_SELL_IN_LIMIT) {
-        item.quality += 2;
-      } else if (item.sellIn <= MID_SELL_IN_LIMIT && item.sellIn > 0) {
-        item.quality += 3;
-      } else {
-        item.quality = 0;
-      }
-    }
-
-    item.sellIn -= 1;
-    return item;
-}
-
-const updateConjured = (item) => {
-  const MIN_ITEM_QUALITY = 0;
-
-  if (item.quality !== MIN_ITEM_QUALITY) {
-    item.sellIn >= 1 ? item.quality -= 2 : item.quality -= 4;
-  }
-
-  item.sellIn -= 1;
-  return item;
 }
